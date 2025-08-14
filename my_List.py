5e67dfa5428dfadd35a498e287008bc32c5e32d6
Allan Musinguzi <allanmusinguzi3443@gmail.com>
2025-08-14 21:26:05 +0300

    Week1 PDS Assignment

---
+++ my_List.py
@@ -0,0 +1,27 @@
+# Create an empty list called my_list
+my_List = []
+
+# Append the following elements to my_list: 10, 20, 30, 40.
+my_List.append(10)
+my_List.append(20)
+my_List.append(30)
+my_List.append(40)
+
+# Insert the value 15 at the second position in the list.
+my_List.insert(1,15)
+
+# Extend my_list with another list: [50, 60, 70].
+
+my_List.extend([50, 60, 70])
+
+# Remove the last element from my_list.
+my_List.pop(-1)
+
+# Sort my_list in ascending order.
+
+my_List.sort()
+
+# Find and print the index of the value 30 in my_list
+my_List.index(30)
+
+print(my_List.index(30))
